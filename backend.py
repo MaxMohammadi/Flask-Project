142da32bdd0651afe5a228913ceb95b2761af288
Max Mohammadi <max.mohammadi@sydereal.com>
2021-01-15 13:51:54 -0800

    backend finished

--- backend.py
+++ backend.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
+import json
+
 app = Flask(__name__)
 
 users = {
@@ -35,18 +38,43 @@ users = {
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    return 'Hello World'
 
-@app.route('/users', methods=['GET', 'POST'])
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
 def get_users():
-    search_username = request.args.get('name')
-    if search_username :
-        subdict = {'users_list' : []}
-        for user in users['users_list']:
-            if user['name'] == search_username:
-                subdict['users_list'].append(user)
-        return subdict
-    return users 
+    if request.method == 'GET':
+        search_username = request.args.get('name')
+        search_job = request.args.get('job')
+        if search_username and search_job :
+            subdict = {'users_list' : []}
+            for user in users['users_list']:
+                if user['name'] == search_username and user['job'] == search_job:
+                    subdict['users_list'].append(user)
+            return subdict
+        elif search_username :
+            subdict = {'users_list' : []}
+            for user in users['users_list']:
+                if user['name'] == search_username:
+                    subdict['users_list'].append(user)
+            return subdict
+        elif search_job:
+            subdict = {'users_list' : []}
+            for user in users['users_list']:
+                if user['job'] ==  search_job:
+                    subdict['users_list'].append(user)
+            return subdict
+        return users
+    elif request.method == 'POST':
+        userToAdd = request.get_json()
+        users['users_list'].append(userToAdd)
+        resp = jsonify(success=True)
+        resp.status_code = 200
+        return resp
+    elif request.method == 'DELETE':
+        userToDelete = request.get_json()
+        users['users_list'].remove(userToDelete)
+        resp = jsonify(success=True)
+        return resp
 
 @app.route('/users/<id>')
 def get_user(id):
