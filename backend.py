ac9f1f3e2bdd6f1c102f4422916dc223b520b2bf
Max Mohammadi <max.mohammadi@sydereal.com>
2021-01-15 20:25:32 -0800

    linking branch commit

--- backend.py
+++ backend.py
@@ -2,8 +2,15 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import json
+from flask_cors import CORS
+import random
 
 app = Flask(__name__)
+CORS(app)
+
+startingID = 10000
+def generateID():
+    return random.randint(10000, 99999)
 
 users = {
     'users_list' :
@@ -44,45 +51,36 @@ def hello_world():
 def get_users():
     if request.method == 'GET':
         search_username = request.args.get('name')
-        search_job = request.args.get('job')
-        if search_username and search_job :
-            subdict = {'users_list' : []}
-            for user in users['users_list']:
-                if user['name'] == search_username and user['job'] == search_job:
-                    subdict['users_list'].append(user)
-            return subdict
-        elif search_username :
+        if search_username:
             subdict = {'users_list' : []}
             for user in users['users_list']:
                 if user['name'] == search_username:
                     subdict['users_list'].append(user)
             return subdict
-        elif search_job:
-            subdict = {'users_list' : []}
-            for user in users['users_list']:
-                if user['job'] ==  search_job:
-                    subdict['users_list'].append(user)
-            return subdict
         return users
     elif request.method == 'POST':
         userToAdd = request.get_json()
+        userToAdd[id] = generateID()
         users['users_list'].append(userToAdd)
         resp = jsonify(success=True)
-        resp.status_code = 200
+        resp.status_code = 201
         return resp
+    else:
+        return 'Invallid HTTP operation'
+
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
+def handle_user(id):
+    if request.method == 'GET':
+        if id :
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return ({})
+        return users
     elif request.method == 'DELETE':
-        userToDelete = request.get_json()
-        users['users_list'].remove(userToDelete)
-        resp = jsonify(success=True)
-        return resp
-
-@app.route('/users/<id>')
-def get_user(id):
-    if id :
-        for user in users['users_list']:
-            if user['id'] == id:
-                return user
-        return ({})
-    return users
-
-
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    users['users_list'].remove(user)
+                    return jsonify(success=True)
+        return jsonify(success=False)
